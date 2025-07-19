74cf9aeb0760d81e271a3d8e22c3682f6a2a7146
808Kamalesh <154341138+808Kamalesh@users.noreply.github.com>
2025-07-19 12:36:07 +0530

    updated

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import icon from "astro-icon";
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  site: "https://808kamalesh.github.io/portfolio1/",
+  site: "https://sunday-pichai.github.io/portfolio1/",
   integrations: [tailwind(), icon()],
 });
